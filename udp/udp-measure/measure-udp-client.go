5612868ec8d137c151a8e09f7c8a3c9c628b4b18
Gui-Lima <ggfl@cin.ufpe.br>
2019-08-27 14:33:25 -0300

    Stdev

--- udp/udp-measure/measure-udp-client.go
+++ udp/udp-measure/measure-udp-client.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"middleware-sockets/util"
 )
 
 func StartClient() {
@@ -11,6 +12,7 @@ func StartClient() {
 	conn, _ := net.DialUDP("udp", nil, addr)
 	defer conn.Close()
 	times := make([]time.Duration, 10000)
+	timesFloated := make([]float64, 10000)
 
 	for i := 0; i < 10000; i++ {
 		buffer := make([]byte, 1024)
@@ -19,12 +21,18 @@ func StartClient() {
 		conn.ReadFrom(buffer)
 		elapsed := time.Since(start)
 		times[i] = elapsed
+		timesFloated[i] = float64(elapsed.Nanoseconds())
 	}
 	sum := int64(0)
 	for _, t := range times {
 		sum += t.Nanoseconds()
 	}
 	mean := sum / 10000
+	std := util.StdDev(timesFloated, float64(mean))
+	fmt.Print("Mean: ")
 	fmt.Print(mean)
+	fmt.Println(" nanoseconds")
+	fmt.Print("Stdev: ")
+	fmt.Print(std)
 	fmt.Println(" nanoseconds")
 }
--- util/util.go
+++ util/util.go
@@ -1,9 +1,19 @@
 package util
 
 import "strings"
+import "math"
 
 func TrimString(text string) string {
 	text = strings.TrimRight(text, "\n")
 	text = strings.TrimRight(text, "\r\n")
 	return text
+}
+
+func StdDev(numbers []float64, mean float64) float64 {
+    total := 0.0
+    for _, number := range numbers {
+        total += math.Pow(number-mean, 2)
+    }
+    variance := total / float64(len(numbers)-1)
+    return math.Sqrt(variance)
 }
